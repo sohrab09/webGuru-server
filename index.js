e2f242675f3c91f417d442b29b13e2db05b255fd
Sohrab Hossain <sohrab.cse9@gmail.com>
2021-06-30 20:58:34 +0600

    data post and data get from database

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(fileUpload());
 
 
 app.get('/', (req, res) => {
-  res.send('Hello Coders, Welcome to Web Guru')
+    res.send('Hello Coders, Welcome to Web Guru')
 })
 
 
@@ -28,12 +28,42 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
 
-  const webBlogCollection = client.db("webGuru").collection("webBlog");
+    const webBlogCollection = client.db("webGuru").collection("webBlog");
+
+    // Add Blog In Database 
+
+    app.post('/admin/post-blog', (req, res) => {
+        const file = req.files.image;
+        const name = req.body.name;
+        const postOwner = req.body.postOwner;
+        const category = req.body.category;
+        const description = req.body.description;
+        const newImage = file.data;
+        const convertImage = newImage.toString("base64");
+        const image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(convertImage, "base64"),
+        };
+        webBlogCollection.insertOne({ name, category, postOwner, description, image })
+            .then((result) => {
+                res.send(result.insertedCount > 0);
+                console.log("Blog added to database")
+            });
+    });
+
+
+    // Get Blog From Database 
+
+    app.get('/blogs', (req, res) => {
+        webBlogCollection.find({}).toArray((err, documents) => {
+            res.send(documents);
+        });
+    });
 
 
 
-
-  console.log("Database Connected");
+    console.log("Database Connected");
 });
 
 
