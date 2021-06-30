572e24e5ddbd29caff29b666c9b3b4be54d20292
Sohrab Hossain <sohrab.cse9@gmail.com>
2021-06-30 16:59:33 +0600

    server is ready

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient;
+const fileUpload = require("express-fileupload");
+const fs = require("fs-extra");
+const ObjectID = require('mongodb').ObjectId;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
+
+const port = 5000
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(fileUpload());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Coders, Welcome to Web Guru')
+})
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cyf7w.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+
+  const webBlogCollection = client.db("webGuru").collection("webBlog");
+
+
+
+
+  console.log("Database Connection Successfully");
+});
+
+
+
+app.listen(process.env.PORT || port)
